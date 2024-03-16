cfa189ea73b41d6db49903d80515e7f85c1d2e41
Nikita Trotner <nikita@ttnr.me>
2024-03-16 21:15:56 +0100

    feat: add burger menu to nav

--- src/ui/src/components/navigator/Navigator.svelte
+++ src/ui/src/components/navigator/Navigator.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
-  import * as Avatar from "$lib/components/ui/avatar/index.js";
   import * as Menubar from "$lib/components/ui/menubar";
   import * as Sheet from "$lib/components/ui/sheet/index.js";
   import Globe from "svelte-radix/Globe.svelte";
+  import HamburgerMenu from "svelte-radix/HamburgerMenu.svelte";
   import { Separator } from "$lib/components/ui/separator/index.js";
   import { Button } from "$lib/components/ui/button/index.js";
   import { languages, setLocale, setLocaleInStorage, t } from "$lib/i18n/index";
+  import { userState } from "$lib/states/user";
+  import type { UserProfile } from "$lib/open-api";
+  import { goto } from "$app/navigation";
+  import { browser } from "$app/environment";
+  import { ROUTES } from "../../lib/routes";
 
   function changeLanguage(locale: string) {
     setLocale(locale);
     setLocaleInStorage(locale);
+    if (browser) {
+      document.documentElement.lang = locale;
+    } 
   }
+
+  function redirect(page: string) {
+    goto(page)
+  }
+
+  let user: UserProfile | undefined = undefined;
+  userState.observable().subscribe((state) => (user = state));
 </script>
 
 <Sheet.Root>
@@ -18,8 +33,10 @@
     <Menubar.Root class="h-12">
       <div class="nav-left">
         <div class="title">
-          <Button variant="link" class="text-lg font-semibold"
-            >{$t("common.nav-title")}</Button
+          <Button
+            on:click={() => redirect(ROUTES.HOME)}
+            variant="link"
+            class="text-lg font-semibold">{$t("common.nav-title")}</Button
           >
         </div>
       </div>
@@ -29,9 +46,21 @@
         </Button>
       </div>
       <div class="nav-right">
-        <Avatar.Root>
-          <Avatar.Fallback>UK</Avatar.Fallback>
-        </Avatar.Root>
+        <Menubar.Menu>
+          <Menubar.Trigger><HamburgerMenu class="h-4 w-4" /></Menubar.Trigger>
+          <Menubar.Content>
+            {#if user?.email}
+              <Menubar.Item>{$t("common.nav-menu.profile")}</Menubar.Item>
+              <Menubar.Item>{$t("common.nav-menu.settings")}</Menubar.Item>
+              <Menubar.Separator />
+              <Menubar.Item>{$t("common.nav-menu.logout")}</Menubar.Item>
+            {:else}
+              <Menubar.Item on:click={() => redirect(ROUTES.LOGIN)}
+                >{$t("common.nav-menu.login")}</Menubar.Item
+              >
+            {/if}
+          </Menubar.Content>
+        </Menubar.Menu>
       </div>
     </Menubar.Root>
   </Sheet.Trigger>
